66401b2868defa769b7f3a9154b27906cb1e7ce6
Jelly <iawj0224@gmail.com>
2022-02-18 12:22:34 +0800

    feat: add infinited scroll component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
+<script setup>
+import axios from "axios";
+import { onMounted, onUnmounted, ref } from "vue";
+import Post from "./Post.vue";
+
+const githubList = ref([]);
+const scrollItem = ref(null);
+
+const getList = () => {
+  axios.get("https://api.github.com/users/vuejs/repos").then((res) => {
+    githubList.value = res.data;
+  });
+};
+
+getList();
+
+const loadMore = () => {
+  githubList.value.forEach((e) => {
+    githubList.value.push(e);
+  });
+};
+
+const handleScroll = (e) => {
+  const element = scrollItem.value;
+  if (element.getBoundingClientRect().bottom < window.innerHeight) {
+    loadMore();
+  }
+};
+</script>
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="title">vuejs repo list：</div>
+    <div class="scrollItem" @scroll="handleScroll">
+      <div ref="scrollItem">
+        <Post :item-data="githubList" />
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  max-width: 1000px;
+  margin: auto;
+  overflow: scroll;
+  padding: 14px 20px;
+  background-color: #e9e5e552;
+  .title {
+    font-size: 30px;
+    font-weight: 500;
+    text-align: left;
+    padding-bottom: 15px;
+  }
+  .scrollItem {
+    height: 916px;
+    overflow-y: scroll;
   }
 }
-</script>
+</style>
---
+++ src/views/Post.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+  itemData: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
+
+<template>
+  <div v-for="item in itemData" :key="item" class="card">
+    <span>{{ item.name }}</span>
+    <span>{{ item.description }}</span>
+    <a :href="item.url">{{ item.url }}</a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  height: 100px;
+  display: grid;
+  text-align: left;
+  padding: 18px 15px;
+  box-shadow: 0 2px 5px #bbbbbb;
+  border-radius: 5px;
+  background-color: #fff;
+  margin: 0 2px 17px 2px;
+  span:nth-child(1) {
+    font-weight: bold;
+  }
+}
+</style>
